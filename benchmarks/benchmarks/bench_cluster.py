907b4f98b7c329489c6b229df408ef81fbe346ec
Schefflera-Arboricola <1509aditi@gmail.com>
2024-01-20 00:37:49 +0530

    added time_square_clustering

---
+++ benchmarks/benchmarks/bench_cluster.py
@@ -0,0 +1,11 @@
+from .common import *
+import networkx as nx
+
+
+class Cluster(Benchmark):
+    params = [(backends), (num_nodes), (edge_prob)]
+    param_names = ["backend", "num_nodes", "edge_prob"]
+
+    def time_square_clustering(self, backend, num_nodes, edge_prob):
+        G = get_cached_gnp_random_graph(num_nodes, edge_prob)
+        _ = nx.square_clustering(G, backend=backend)
