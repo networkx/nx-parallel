066f4caf9a81f3ecd0d806c01009308fe04ce512
Schefflera-Arboricola <1509aditi@gmail.com>
2024-01-23 22:47:28 +0530

    initial commit resolved conflicts

--- nx_parallel/algorithms/shortest_paths/weighted.py
+++ nx_parallel/algorithms/shortest_paths/weighted.py
@@ -1,8 +1,15 @@
 from joblib import Parallel, delayed
-from networkx.algorithms.shortest_paths.weighted import single_source_bellman_ford_path
+from networkx.algorithms.shortest_paths.weighted import (
+    _weight_function,
+    _dijkstra,
+    _bellman_ford,
+    single_source_bellman_ford_path,
+)
 import nx_parallel as nxp
 
-__all__ = ["all_pairs_bellman_ford_path"]
+__all__ = [
+    "all_pairs_bellman_ford_path",
+    "johnson",]
 
 
 def all_pairs_bellman_ford_path(G, weight="weight"):
@@ -26,3 +33,34 @@ def all_pairs_bellman_ford_path(G, weight="weight"):
         delayed(_calculate_shortest_paths_subset)(source) for source in nodes
     )
     return paths
+
+def johnson(G, weight="weight"):
+    """Uses Johnson's Algorithm to compute shortest paths."""
+    dist = {v: 0 for v in G}
+    pred = {v: [] for v in G}
+    weight = _weight_function(G, weight)
+
+    # Calculate distance of shortest paths
+    dist_bellman = _bellman_ford(G, list(G), weight, pred=pred, dist=dist)
+
+    # Update the weight function to take into account the Bellman--Ford
+    # relaxation distances.
+    def new_weight(u, v, d):
+        return weight(u, v, d) + dist_bellman[u] - dist_bellman[v]
+
+    def dist_path(v):
+        paths = {v: [v]}
+        _dijkstra(G, v, new_weight, paths=paths)
+        return paths
+
+    def _johnson_subset(chunk):
+        return {node: dist_path(node) for node in chunk}
+
+    total_cores = nxp.cpu_count()
+    num_in_chunk = max(len(G.nodes) // total_cores, 1)
+    node_chunks = nxp.chunks(G.nodes, num_in_chunk)
+
+    results = Parallel(n_jobs=total_cores)(
+        delayed(_johnson_subset)(chunk) for chunk in node_chunks
+    )
+    return {v: d_path for result_chunk in results for v, d_path in result_chunk.items()}
