c1b37e4a2ff281076b222e1dc09ced85e98b4aa7
Schefflera-Arboricola <1509aditi@gmail.com>
2024-02-12 12:30:56 +0530

    added benchmarks for reaching centrality algos

--- benchmarks/benchmarks/bench_centrality.py
+++ benchmarks/benchmarks/bench_centrality.py
@@ -6,6 +6,7 @@ from .common import (
     Benchmark,
 )
 import networkx as nx
+import random
 
 
 class Betweenness(Benchmark):
@@ -15,3 +16,17 @@ class Betweenness(Benchmark):
     def time_betweenness_centrality(self, backend, num_nodes, edge_prob):
         G = get_cached_gnp_random_graph(num_nodes, edge_prob)
         _ = nx.betweenness_centrality(G, backend=backend)
+
+
+class Reaching(Benchmark):
+    params = [(backends), (num_nodes), (edge_prob)]
+    param_names = ["backend", "num_nodes", "edge_prob"]
+
+    def time_global_reaching_centrality(self, backend, num_nodes, edge_prob):
+        G = get_cached_gnp_random_graph(num_nodes, edge_prob, is_weighted=True, directed=True)
+        _ = nx.global_reaching_centrality(G, weight="weight", backend=backend)
+
+    def time_local_reaching_centrality(self, backend, num_nodes, edge_prob):
+        G = get_cached_gnp_random_graph(num_nodes, edge_prob, is_weighted=True, directed=True)
+        v = random.randint(0, num_nodes-1)
+        _ = nx.local_reaching_centrality(G, v, weight="weight", backend=backend)
