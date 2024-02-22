bac878ae36b62667697b22c93dfe8be67189a777
Schefflera-Arboricola <1509aditi@gmail.com>
2024-02-23 00:31:20 +0530

    updated all_pairs_bellman_ford_path

--- nx_parallel/algorithms/shortest_paths/weighted.py
+++ nx_parallel/algorithms/shortest_paths/weighted.py
@@ -2,27 +2,26 @@ from joblib import Parallel, delayed
 from networkx.algorithms.shortest_paths.weighted import single_source_bellman_ford_path
 import nx_parallel as nxp
 
-__all__ = ["all_pairs_bellman_ford_path"]
+__all__ = [
+    "all_pairs_bellman_ford_path",
+]
 
 
 def all_pairs_bellman_ford_path(G, weight="weight"):
-    """The parallel computation is implemented by computing the
-    shortest paths for each node concurrently.
+    """This parallel implementation first creates a generator to lazily compute
+    shortest paths for each node, and then employs joblib's `Parallel` function
+    to execute these computations in parallel across all available CPU cores.
 
     networkx.all_pairs_bellman_ford_path : https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.weighted.all_pairs_bellman_ford_path.html#all-pairs-bellman-ford-path
     """
 
-    def _calculate_shortest_paths_subset(source):
+    def _process_node(source):
         return (source, single_source_bellman_ford_path(G, source, weight=weight))
 
     if hasattr(G, "graph_object"):
         G = G.graph_object
 
-    cpu_count = nxp.cpu_count()
+    paths_generator = (delayed(_process_node)(source) for source in G.nodes)
 
-    nodes = G.nodes
-
-    paths = Parallel(n_jobs=cpu_count, return_as="generator")(
-        delayed(_calculate_shortest_paths_subset)(source) for source in nodes
-    )
-    return paths
+    for path in Parallel(n_jobs=nxp.cpu_count())(paths_generator):
+        yield path
