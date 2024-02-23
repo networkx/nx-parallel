8e9390a03875217da2b62a2b08a6eb0155ff2a3a
Schefflera-Arboricola <1509aditi@gmail.com>
2024-02-23 18:42:11 +0530

    used chunks and added get_chunks to all_pairs_bellman_ford_path

--- nx_parallel/algorithms/shortest_paths/weighted.py
+++ nx_parallel/algorithms/shortest_paths/weighted.py
@@ -7,21 +7,43 @@ __all__ = [
 ]
 
 
-def all_pairs_bellman_ford_path(G, weight="weight"):
-    """This parallel implementation first creates a generator to lazily compute
-    shortest paths for each node, and then employs joblib's `Parallel` function
-    to execute these computations in parallel across all available CPU cores.
+def all_pairs_bellman_ford_path(G, weight="weight", get_chunks=None):
+    """The parallel implementation first divides the nodes into chunks and then
+    creates a generator to lazily compute shortest paths for each node_chunk, and
+    then employs joblib's `Parallel` function to execute these computations in
+    parallel across all available CPU cores.
+
+    Parameters 
+    ------------
+    get_chunks : function (default = None)
+        A function that takes in an iterable of all the nodes as input and returns
+        an iterable `node_chunks`
 
     networkx.all_pairs_bellman_ford_path : https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.weighted.all_pairs_bellman_ford_path.html#all-pairs-bellman-ford-path
     """
 
-    def _process_node(source):
-        return (source, single_source_bellman_ford_path(G, source, weight=weight))
+    def _process_node_chunk(node_chunk):
+        return [
+            (node, single_source_bellman_ford_path(G, node, weight=weight))
+            for node in node_chunk
+        ]
 
     if hasattr(G, "graph_object"):
         G = G.graph_object
 
-    paths_generator = (delayed(_process_node)(source) for source in G.nodes)
+    nodes = G.nodes
+    total_cores = nxp.cpu_count()
 
-    for path in Parallel(n_jobs=nxp.cpu_count())(paths_generator):
-        yield path
+    if get_chunks:
+        node_chunks = get_chunks(nodes)
+    else:
+        num_in_chunk = max(len(nodes) // total_cores, 1)
+        node_chunks = nxp.chunks(nodes, num_in_chunk)
+
+    paths_chunk_generator = (
+        delayed(_process_node_chunk)(node_chunk) for node_chunk in node_chunks
+    )
+
+    for path_chunk in Parallel(n_jobs=nxp.cpu_count())(paths_chunk_generator):
+        for path in path_chunk:
+            yield path
