9d724659404b2fcacd4f8d74dd2e32b4abd85d56
Schefflera-Arboricola <1509aditi@gmail.com>
2024-03-13 19:04:14 +0530

    updated get_chunk pytest

--- nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
+++ nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
@@ -1,12 +1,11 @@
 import networkx as nx
 import nx_parallel as nxp
-import time
 import math
 
 
 def test_betweenness_centrality_get_chunks():
     def get_chunk(nodes):
-        num_chunks = 8  # number of chunks/cores
+        num_chunks = nxp.cpu_count()
         nodes_ebc = {i: 0 for i in nodes}
         for i in ebc:
             nodes_ebc[i[0]] += ebc[i]
@@ -24,17 +23,13 @@ def test_betweenness_centrality_get_chunks():
 
         return chunks
 
-    G = nx.bipartite.random_graph(400, 700, 0.8, seed=5, directed=False)
+    G = nx.fast_gnp_random_graph(100, 0.1, directed=False)
     H = nxp.ParallelGraph(G)
     ebc = nx.edge_betweenness_centrality(G)
-    t1 = time.time()
     par_bc_chunk = nxp.betweenness_centrality(H, get_chunks=get_chunk)  # smoke test
-    t2 = time.time()
     par_bc = nxp.betweenness_centrality(H)
-    t3 = time.time()
+
     for i in range(len(G.nodes)):
         assert math.isclose(par_bc[i], par_bc_chunk[i], abs_tol=1e-16)
-    with_chunk = t2 - t1
-    without_chunk = t3 - t2
-    assert with_chunk < without_chunk
     # get_chunk is faster than default(for big graphs)
+    # G = nx.bipartite.random_graph(400, 700, 0.8, seed=5, directed=False)
