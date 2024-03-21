72d4dd5c39c0a8203f7668ef5c9ddedf3b9334eb
Schefflera-Arboricola <1509aditi@gmail.com>
2024-03-22 01:09:24 +0530

    init commit

--- _nx_parallel/update_get_info.py
+++ _nx_parallel/update_get_info.py
@@ -132,5 +132,5 @@ def get_info():
         "short_summary": "Parallel backend for NetworkX algorithms",
         "functions": '''
 
-with open("_nx_parallel/temp__init__.py", "w") as f:
+with open("_nx_parallel/__init__.py", "w") as f:
     f.write(string + str(get_funcs_info()) + "}\n")
