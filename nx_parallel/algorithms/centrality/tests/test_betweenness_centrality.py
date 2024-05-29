e0d937fa61df344fed292867be3a0a124c2b7cf8
Jakub Krajniak <jkrajniak@gmail.com>
2024-05-29 20:38:50 +0200

    put back get_chunk_function

--- nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
+++ nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
@@ -3,30 +3,29 @@ import nx_parallel as nxp
 import math
 
 
-def get_chunk_function(ebc):
-    def get_chunk(nodes):
-        num_chunks = nxp.cpu_count()
-        nodes_ebc = {i: 0 for i in nodes}
-        for i in ebc:
-            nodes_ebc[i[0]] += ebc[i]
-            nodes_ebc[i[1]] += ebc[i]
-
-        sorted_nodes = sorted(nodes_ebc.items(), key=lambda x: x[1], reverse=True)
-
-        chunks = [[] for _ in range(num_chunks)]
-        chunk_sums = [0] * num_chunks
-
-        for node, value in sorted_nodes:
-            min_chunk_index = chunk_sums.index(min(chunk_sums))
-            chunks[min_chunk_index].append(node)
-            chunk_sums[min_chunk_index] += value
-
-        return chunks
-
-    return get_chunk
-
-
 def test_betweenness_centrality_get_chunks():
+    def get_chunk_function(ebc):
+        def get_chunk(nodes):
+            num_chunks = nxp.cpu_count()
+            nodes_ebc = {i: 0 for i in nodes}
+            for i in ebc:
+                nodes_ebc[i[0]] += ebc[i]
+                nodes_ebc[i[1]] += ebc[i]
+
+            sorted_nodes = sorted(nodes_ebc.items(), key=lambda x: x[1], reverse=True)
+
+            chunks = [[] for _ in range(num_chunks)]
+            chunk_sums = [0] * num_chunks
+
+            for node, value in sorted_nodes:
+                min_chunk_index = chunk_sums.index(min(chunk_sums))
+                chunks[min_chunk_index].append(node)
+                chunk_sums[min_chunk_index] += value
+
+            return chunks
+
+        return get_chunk
+
     G = nx.fast_gnp_random_graph(100, 0.1, directed=False)
     H = nxp.ParallelGraph(G)
     ebc = nx.edge_betweenness_centrality(G)
