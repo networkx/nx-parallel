8164d5a6e8d4a89960fc582498f6620898948cec
Aditi Juneja <91629733+Schefflera-Arboricola@users.noreply.github.com>
2024-06-12 15:43:20 +0530

    Update test_betweenness_centrality.py

--- nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
+++ nx_parallel/algorithms/centrality/tests/test_betweenness_centrality.py
@@ -4,34 +4,29 @@ import math
 
 
 def test_betweenness_centrality_get_chunks():
-    def get_chunk_function(ebc):
-        def get_chunk(nodes):
-            num_chunks = nxp.cpu_count()
-            nodes_ebc = {i: 0 for i in nodes}
-            for i in ebc:
-                nodes_ebc[i[0]] += ebc[i]
-                nodes_ebc[i[1]] += ebc[i]
+    def get_chunk(nodes):
+        num_chunks = nxp.cpu_count()
+        nodes_ebc = {i: 0 for i in nodes}
+        for i in ebc:
+            nodes_ebc[i[0]] += ebc[i]
+            nodes_ebc[i[1]] += ebc[i]
 
-            sorted_nodes = sorted(nodes_ebc.items(), key=lambda x: x[1], reverse=True)
+        sorted_nodes = sorted(nodes_ebc.items(), key=lambda x: x[1], reverse=True)
 
-            chunks = [[] for _ in range(num_chunks)]
-            chunk_sums = [0] * num_chunks
+        chunks = [[] for _ in range(num_chunks)]
+        chunk_sums = [0] * num_chunks
 
-            for node, value in sorted_nodes:
-                min_chunk_index = chunk_sums.index(min(chunk_sums))
-                chunks[min_chunk_index].append(node)
-                chunk_sums[min_chunk_index] += value
+        for node, value in sorted_nodes:
+            min_chunk_index = chunk_sums.index(min(chunk_sums))
+            chunks[min_chunk_index].append(node)
+            chunk_sums[min_chunk_index] += value
 
-            return chunks
-
-        return get_chunk
+        return chunks
 
     G = nx.fast_gnp_random_graph(100, 0.1, directed=False)
     H = nxp.ParallelGraph(G)
     ebc = nx.edge_betweenness_centrality(G)
-    par_bc_chunk = nxp.betweenness_centrality(
-        H, get_chunks=get_chunk_function(ebc)
-    )  # smoke test
+    par_bc_chunk = nxp.betweenness_centrality(H, get_chunks=get_chunk)  # smoke test
     par_bc = nxp.betweenness_centrality(H)
 
     for i in range(len(G.nodes)):
