d19afa19922db5dd01ddc7384ac525a58ac2410e
Aditi Juneja <1509aditi@gmail.com>
2024-08-04 22:54:18 +0530

    test

--- nx_parallel/algorithms/cluster.py
+++ nx_parallel/algorithms/cluster.py
@@ -8,7 +8,7 @@ __all__ = [
 
 
 def square_clustering(G, nodes=None, get_chunks="chunks"):
-    """The nodes are chunked into `node_chunks` and then the square clustering
+    """blah The nodes are chunked into `node_chunks` and then the square clustering
     coefficient for all `node_chunks` are computed in parallel over all available
     CPU cores.
 
