b8f641e3123786ba1a4a391267f39c5be791e37c
Aditi Juneja <1509aditi@gmail.com>
2024-08-05 13:49:26 +0530

    creating venv and using echo to debug

--- _nx_parallel/script.sh
+++ _nx_parallel/script.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-python3 _nx_parallel/update_get_info.py
+echo "inside script"
+# Create a temporary virtual environment
+python3 -m venv temp_env
 
-ruff format "_nx_parallel/temp__init__.py"
+# Activate the virtual environment
+source temp_env/bin/activate 
+
+pip install ruff==0.1.8
+
+python3 _nx_parallel/update_get_info.py
+echo "updated"
+echo "pip list"
+pip list
+echo "ruff version:"
+ruff --version
+echo "which ruff:"
+which ruff
+ruff format "_nx_parallel/temp__init__.py" --verbose
+echo "formatted"
 
 # Check if there's any difference between the original file and the formatted one
 if diff -q "_nx_parallel/__init__.py" "_nx_parallel/temp__init__.py" >/dev/null; then
+    echo "no changes"
     rm "_nx_parallel/temp__init__.py"
+    echo "temp removed"
 else
+    echo "changes"
     mv "_nx_parallel/temp__init__.py" "_nx_parallel/__init__.py"
+    echo "moved"
 fi
+
+echo "done"
+
+deactivate
+
+rm -rf temp_env
