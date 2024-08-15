9dae3089162d1b6c5849f9f6dd223c79cc1c9878
Aditi Juneja <1509aditi@gmail.com>
2024-08-15 17:27:10 +0530

    updated test_get_chunks

--- nx_parallel/tests/test_get_chunks.py
+++ nx_parallel/tests/test_get_chunks.py
@@ -58,6 +58,10 @@ def test_get_chunks():
         "betweenness_centrality",
         "edge_betweenness_centrality",
     ]
+    dag_chk_list_vals = [
+        "colliders",
+        "v_structures",
+    ]
     G = nx.fast_gnp_random_graph(50, 0.6, seed=42)
     H = nxp.ParallelGraph(G)
     for func in get_chunks_funcs:
@@ -73,12 +77,16 @@ def test_get_chunks():
                 c1 = getattr(nxp, func)(H)
                 c2 = getattr(nxp, func)(H, get_chunks=random_chunking)
                 if isinstance(c1, types.GeneratorType):
-                    c1, c2 = dict(c1), dict(c2)
                     if func in chk_dict_vals:
+                        c1, c2 = dict(c1), dict(c2)
                         for i in range(len(G.nodes)):
                             assert math.isclose(c1[i], c2[i], abs_tol=1e-16)
-                    else:
-                        assert c1 == c2
+                    elif func in dag_chk_list_vals:
+                        G_dir = nx.DiGraph(G)
+                        H_dir = nxp.ParallelGraph(G_dir)
+                        c1 = getattr(nxp, func)(H_dir)
+                        c2 = getattr(nxp, func)(H_dir, get_chunks=random_chunking)
+                        assert sorted(list(c1)) == sorted(list(c2))
                 else:
                     if func in chk_dict_vals:
                         for i in range(len(G.nodes)):
