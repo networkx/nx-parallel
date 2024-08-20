5907c0445a07a9157c5ad36d50ae5237918e163c
Aditi Juneja <1509aditi@gmail.com>
2024-08-21 01:01:01 +0530

    ignoring functions in utils

--- _nx_parallel/__init__.py
+++ _nx_parallel/__init__.py
@@ -87,29 +87,11 @@ def get_info():
                     'get_chunks : str, function (default = "chunks")': "A function that takes in a list of all the nodes as input and returns an iterable `node_chunks`. The default chunking is done by slicing the `nodes` into `n` chunks, where `n` is the number of CPU cores."
                 },
             },
-            "chunks": {
-                "url": "https://github.com/networkx/nx-parallel/blob/main/nx_parallel/utils/chunk.py#L8",
-                "additional_docs": "Divides an iterable into chunks of size n",
-                "additional_parameters": None,
-            },
             "closeness_vitality": {
                 "url": "https://github.com/networkx/nx-parallel/blob/main/nx_parallel/algorithms/vitality.py#L9",
                 "additional_docs": "The parallel computation is implemented only when the node is not specified. The closeness vitality for each node is computed concurrently.",
                 "additional_parameters": {
                     'get_chunks : str, function (default = "chunks")': "A function that takes in a list of all the nodes as input and returns an iterable `node_chunks`. The default chunking is done by slicing the `nodes` into `n` chunks, where `n` is the total number of CPU cores."
-                },
-            },
-            "cpu_count": {
-                "url": "https://github.com/networkx/nx-parallel/blob/main/nx_parallel/utils/chunk.py#L18",
-                "additional_docs": "Returns the number of logical CPUs or cores",
-                "additional_parameters": None,
-            },
-            "create_iterables": {
-                "url": "https://github.com/networkx/nx-parallel/blob/main/nx_parallel/utils/chunk.py#L26",
-                "additional_docs": "Creates an iterable of function inputs for parallel computation based on the provided iterator type.",
-                "additional_parameters": {
-                    "G : NetworkX graph": "iterator : str Type of iterator. Valid values are 'node', 'edge', 'isolate'",
-                    "iterable : Iterable": "An iterable of function inputs.",
                 },
             },
             "edge_betweenness_centrality": {
