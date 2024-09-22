cb69e266867c7d29932e631876516761fa3d4ad6
Derek Alexander <derekpisner@dpisner-MB.local>
2024-09-22 01:36:59 -0400

    chore: .gitignore

--- .history/nx_parallel/utils/types_20240914134128.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class GraphIteratorType(Enum):
-    NODE = "nodes"
-    EDGE = "edges"
-    ISOLATE = "isolates"
--- .history/nx_parallel/utils/types_20240914134134.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class GraphIteratorType(Enum):
-    NODES = "nodes"
-    EDGES = "edges"
-    ISOLATES = "isolates"
