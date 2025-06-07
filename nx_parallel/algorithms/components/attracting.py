9d12ce514cdcbfb3757039413aeabf2042be352f
Akshita Sure <sureakshita23@gmail.com>
2025-06-07 14:50:11 +0530

    added parallel implementation of number_connected_components

--- nx_parallel/algorithms/components/attracting.py
+++ nx_parallel/algorithms/components/attracting.py
@@ -10,8 +10,8 @@ __all__ = ["number_attracting_components"]
 @nxp._configure_if_nx_active()
 def number_attracting_components(G, get_chunks="chunks"):
     """The parallel computation is implemented by dividing the list
-    of nodes into chunks and then finding the length of each chunk in parallel
-    and then adding all the lengths at the end.
+    of attracting components into chunks and then finding the length
+    of each chunk in parallel and then adding all the lengths at the end.
 
     networkx.number_attracting_components : https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.components.number_attracting_components.html
 
@@ -19,7 +19,7 @@ def number_attracting_components(G, get_chunks="chunks"):
     ----------
     get_chunks : str, function (default = "chunks")
         A function that takes in a list of attracting components as input and returns
-        an iterable `node_iter_chunks`. The default chunking is done by slicing the
+        an iterable `component_chunks`. The default chunking is done by slicing the
         list of attracting components into `n_jobs` number of chunks.
     """
     if hasattr(G, "graph_object"):
@@ -29,9 +29,9 @@ def number_attracting_components(G, get_chunks="chunks"):
 
     attracting_comp_list = list(attracting_components(G))
     if get_chunks == "chunks":
-        node_iter_chunks = nxp.chunks(attracting_comp_list, n_jobs)
+        component_chunks = nxp.chunks(attracting_comp_list, n_jobs)
     else:
-        node_iter_chunks = get_chunks(attracting_comp_list)
+        component_chunks = get_chunks(attracting_comp_list)
 
-    results = Parallel()(delayed(len)(chunk) for chunk in node_iter_chunks)
+    results = Parallel()(delayed(len)(chunk) for chunk in component_chunks)
     return sum(results)
