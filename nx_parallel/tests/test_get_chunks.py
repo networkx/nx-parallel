0dd25172dbb429998604e30c121773d065c3e513
akshita <145579024+akshitasure12@users.noreply.github.com>
2025-08-14 00:36:29 +0530

    Update `test_get_chunks` for new algorithms (#129)
    * Fix test_get_chunks to handle community link prediction algorithms
    
    * Fix lint
    
    * modify to add a simpler test
    
    * remove code redundancy
    
    * minor edit: use G
    
    * Improved test to remove redundancy
    
    * fixed empty generator issue

--- nx_parallel/tests/test_get_chunks.py
+++ nx_parallel/tests/test_get_chunks.py
@@ -11,6 +11,7 @@ import pytest
 
 import nx_parallel as nxp
 from nx_parallel.interface import ALGORITHMS
+from collections.abc import Iterable
 
 
 def get_functions_with_get_chunks(ignore_funcs=[], package_name="nx_parallel"):
@@ -52,35 +53,61 @@ def test_get_chunks(func):
     tournament_funcs = [
         "tournament_is_strongly_connected",
     ]
+    requires_node_community = [
+        "cn_soundarajan_hopcroft",
+        "ra_index_soundarajan_hopcroft",
+        "within_inter_cluster",
+    ]
     check_dict_values_close = [
         "betweenness_centrality",
         "edge_betweenness_centrality",
     ]
+    not_implemented_undirected = [
+        "number_attracting_components",
+        "number_weakly_connected_components",
+        "number_strongly_connected_components",
+        "v_structures",
+        "colliders",
+    ]
 
     if func in tournament_funcs:
         G = nx.tournament.random_tournament(15, seed=42)
-        H = nxp.ParallelGraph(G)
-        c1 = getattr(nxp, func)(H)
-        c2 = getattr(nxp, func)(H, get_chunks=random_chunking)
-        assert c1 == c2
     else:
-        G = nx.fast_gnp_random_graph(40, 0.6, seed=42)
-        H = nxp.ParallelGraph(G)
+        G = nx.fast_gnp_random_graph(
+            40, 0.6, seed=42, directed=func in not_implemented_undirected
+        )
+
+    H = nxp.ParallelGraph(G)
+
+    if func in requires_node_community:
+        nx.set_node_attributes(G, {i: i % 2 for i in G.nodes}, "community")
+        ebunch = [(0, 3)]
+        c1 = getattr(nxp, func)(H, ebunch)
+        c2 = getattr(nxp, func)(H, ebunch, get_chunks=random_chunking)
+    else:
         c1 = getattr(nxp, func)(H)
         c2 = getattr(nxp, func)(H, get_chunks=random_chunking)
-        if isinstance(c1, types.GeneratorType):
-            c1, c2 = dict(c1), dict(c2)
-            if func in check_dict_values_close:
-                for key in c1:
-                    assert math.isclose(c1[key], c2[key], abs_tol=1e-16)
-            else:
-                assert c1 == c2
+
+    if isinstance(c1, types.GeneratorType):
+        c1_list, c2_list = list(c1), list(c2)
+        try:
+            c1, c2 = dict(c1_list), dict(c2_list)
+        except ValueError:
+            assert sorted(c1_list) == sorted(c2_list)
         else:
             if func in check_dict_values_close:
                 for key in c1:
                     assert math.isclose(c1[key], c2[key], abs_tol=1e-16)
             else:
-                if isinstance(c1, float):
-                    assert math.isclose(c1, c2, abs_tol=1e-16)
-                else:
-                    assert c1 == c2
+                assert c1 == c2
+    else:
+        if func in check_dict_values_close:
+            for key in c1:
+                assert math.isclose(c1[key], c2[key], abs_tol=1e-16)
+        else:
+            if isinstance(c1, float):
+                assert math.isclose(c1, c2, abs_tol=1e-16)
+            elif isinstance(c1, Iterable):
+                assert sorted(c1) == sorted(c2)
+            else:
+                assert c1 == c2
