49690b25ca05cf3efc925ac61d6342f291d61fe0
Akshita Sure <sureakshita23@gmail.com>
2025-08-15 17:29:35 +0530

    use dag instead of directed graph

--- benchmarks/benchmarks/bench_dag.py
+++ benchmarks/benchmarks/bench_dag.py
@@ -1,22 +1,20 @@
 from .common import (
     backends,
     num_nodes,
-    edge_prob,
-    get_cached_gnp_random_graph,
     Benchmark,
 )
 import networkx as nx
 
 
 class DAG(Benchmark):
-    params = [backends, num_nodes, edge_prob]
-    param_names = ["backend", "num_nodes", "edge_prob"]
+    params = [backends, num_nodes]
+    param_names = ["backend", "num_nodes"]
 
-    def setup(self, backend, num_nodes, edge_prob):
-        self.G = get_cached_gnp_random_graph(num_nodes, edge_prob, is_directed=True)
+    def setup(self, backend, num_nodes):
+        self.G = nx.gn_graph(num_nodes, seed=42, create_using=nx.DiGraph)
 
-    def time_colliders(self, backend, num_nodes, edge_prob):
+    def time_colliders(self, backend, num_nodes):
         _ = nx.dag.colliders(self.G, backend=backend)
 
-    def time_v_structures(self, backend, num_nodes, edge_prob):
+    def time_v_structures(self, backend, num_nodes):
         _ = nx.dag.v_structures(self.G, backend=backend)
