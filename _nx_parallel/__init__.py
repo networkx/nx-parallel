6a90f3573b7159df8c6eac9751f890eda0a284e8
Gulshan Kumar <gksept2005@gmail.com>
2026-01-11 09:45:46 +0530

    style

--- _nx_parallel/__init__.py
+++ _nx_parallel/__init__.py
@@ -183,9 +183,9 @@ def get_info():
             },
             "latapy_clustering": {
                 "url": "https://github.com/networkx/nx-parallel/blob/main/nx_parallel/algorithms/bipartite/cluster.py#L10",
-                "additional_docs": "The parallel computation is implemented by dividing the nodes into chunks and computing the bipartite clustering coefficient for each chunk in parallel.",
+                "additional_docs": "In the parallel implementation we divide the nodes into chunks and compute the bipartite clustering coefficient for all `node_chunk` in parallel.",
                 "additional_parameters": {
-                    'get_chunks : str, function (default = "chunks")': "A function that takes in an iterable of all the nodes as input and returns an iterable `node_chunks`. The default chunking is done by slicing the `G.nodes` into `n_jobs` number of chunks."
+                    'get_chunks : str, function (default = "chunks")': "A function that takes in an iterable of all the nodes as input and returns an iterable `node_chunks`. The default chunking is done by slicing the `G.nodes` (or `nodes`) into `n_jobs` number of chunks."
                 },
             },
             "local_efficiency": {
